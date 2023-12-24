3fac40d94a00fd1be9650756b3636279641deb47
S.D.L Abhishek Pramuditha <153332055+Abhishek2003SD@users.noreply.github.com>
2023-12-24 16:15:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="myStyle.css">
+</head>
+
+<body id="bgbody">
+
+    <!-- design Interface -->
+    <div class="btn">
+        <button id="display1" onclick="myTeastFunction1()">DarkMode</button>
+        <button id="display2" onclick="myTeastFunction2()">LightMode</button>
+    </div>
+
+    <div class="content">
+        <h1 id="h1">Hello Word!</h1>
+        <h4 id="h2">This is a sample text</h4>
+    </div>
+
+
+    <div class="end">
+        <h1 id="h3">Mode:</h1>
+        <h3 id="h4">Normal</h3>
+    </div>
+
+
+    <!-- call the javaScript function -->
+    <script src="myJscript.js"></script>
+
+</body>
+</html>
---
+++ myJscript.js
@@ -0,0 +1,25 @@
+// create the function for light mode and dark mode buttons
+
+
+// function for Darkmode button
+function myTeastFunction1() {
+    document.getElementById('h4').innerHTML = ("Dark Mode");
+    h4.style.color="blue";
+    bgbody.style.backgroundColor="black";
+    h1.style.color="white";
+    h2.style.color="white";
+    h3.style.color="white";
+
+}
+
+// function for light mode button
+function myTeastFunction2() {
+    document.getElementById('h4').innerHTML = ("Light Mode");
+    h4.style.color="red";
+    bgbody.style.backgroundColor="white";
+    h1.style.color="black";
+    h2.style.color="black";
+    h3.style.color="black";
+}
+
+// this functions are calling index.php
